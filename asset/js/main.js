69b776712c5783fea4fdeaa6f0bec059de4c0de7
Your Name <khongmuonroixa98@gmail.com>
2021-06-27 17:13:31 +0700

    first

---
+++ asset/js/main.js
@@ -0,0 +1,20 @@
+// slider
+// $(document).ready(function () {
+//     $(".slider__items").slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//         responsive: [{
+
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 1
+//             }
+//         }],
+
+//     });
+// });
+
+// slider
+
